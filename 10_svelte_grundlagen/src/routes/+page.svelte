f987e195745b926fa6856fc64d2535033df6d19d
Benjamin Thomsen <info@benjaminthomsen.de>
2026-04-21 12:13:37 +0200

    points change

--- 10_svelte_grundlagen/src/routes/+page.svelte
+++ 10_svelte_grundlagen/src/routes/+page.svelte
@@ -18,7 +18,7 @@
 
 		<div class="svg-container">
 			<svg class="svg-canvas" viewBox="0 0 1000 1000">
-				<polygon points="0,0 200,0 200,50 0,50" style="fill:#0085ff"></polygon>
+				<polygon points="0,0 200,0 200,50 0,50" fill="#0085ff"></polygon>
 			</svg>
 		</div>
 	</main>
