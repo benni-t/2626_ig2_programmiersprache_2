3a5b43b930528651f447b0f6dbdea7135a413b7f
Benjamin Thomsen <info@benjaminthomsen.de>
2026-04-28 12:44:41 +0200

    change page comp pattern path

--- 11_svelte_pattern/src/routes/+page.svelte
+++ 11_svelte_pattern/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import Header from '$lib/components/Header.svelte'
   import PatternNavigation from '$lib/components/PatternNavigation.svelte'
-  import Pattern from '$lib/components/Pattern1_4.svelte'
+  import Pattern from '$lib/components/Pattern1_3.svelte'
   import Footer from '$lib/components/Footer.svelte'
 </script>
 
