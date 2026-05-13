dda63c3e2fe8cb9189d33effe8270e3e53de4ca7
Benjamin Thomsen <info@benjaminthomsen.de>
2026-05-13 12:01:17 +0200

    added(page): pattern navigation

---
+++ 11_svelte_pattern/src/lib/components/Pattern1_4.svelte
@@ -0,0 +1,41 @@
+<script>
+
+	// Selber machen lassen, nun können Sie Positionen über Funktionen und Modulo-Operatoren berechnen, um komplexere Muster zu erstellen.
+	const elementWidth = 50;
+	const elementCountX = 1000 / elementWidth;
+
+	const elementHeight = 100;
+	const elementCountY = (1000 / elementHeight) * 2;
+
+	function getColor1(xi, yi) {
+		if (yi % 2 == 0 && xi % 2 == 0) return 'red'; 
+		if (yi % 2 == 0 && xi % 2 == 1) return 'blue';
+		if (yi % 2 == 1 && xi % 2 == 0) return 'yellow'; 
+		if (yi % 2 == 1 && xi % 2 == 1) return 'green'; 
+	}
+
+	function calculatePosition(xi, yi) {
+		if (yi % 2 == 0) {
+			let x = xi * elementWidth;
+			let y = yi * (elementHeight / 2);
+			return `translate(${x} ${y})`;
+		} else {
+			let x = xi * elementWidth - elementWidth / 2;
+			let y = yi * (elementHeight / 2);
+			return `translate(${x} ${y})`;
+		}
+	}
+</script>
+
+<div class="svg-container">
+	<svg class="svg-canvas" viewBox="0 0 1000 1000">
+		{#each Array(elementCountY) as _, yi}
+			{#each Array(elementCountX) as _, xi}
+				<g transform={calculatePosition(xi, yi)}>
+					<polygon points="0,{elementHeight / 2} {elementWidth / 2},0 {elementWidth},{elementHeight /
+							2} {elementWidth / 2}, {elementHeight}" fill={getColor1(xi, yi)}></polygon>
+				</g>
+			{/each}
+		{/each}
+	</svg>
+</div>
--- 11_svelte_pattern/src/routes/+page.svelte
+++ 11_svelte_pattern/src/routes/+page.svelte
@@ -1,17 +1,115 @@
 <script>
-  import Header from '$lib/components/Header.svelte'
-  import PatternNavigation from '$lib/components/PatternNavigation.svelte'
-  import Pattern from '$lib/components/Pattern1_3.svelte'
-  import Footer from '$lib/components/Footer.svelte'
+	import { slide } from 'svelte/transition';
+	import Header from '$lib/components/Header.svelte';
+
+	// Import für die einzelnen Patterns
+	import Pattern1 from '$lib/components/Pattern1_1.svelte';
+	import Pattern2 from '$lib/components/Pattern1_2.svelte';
+	import Pattern3 from '$lib/components/Pattern1_3.svelte';
+	import Pattern4 from '$lib/components/Pattern1_4.svelte';
+
+	// Import der Navigation kann entfernt werden, die Navigation ist nun direkt hier drin (machts einfacher)
+	// import PatternNavigation from '$lib/components/PatternNavigation.svelte'
+
+	import Footer from '$lib/components/Footer.svelte';
+
+	// Array mit den Patterns als Objekte, mit 3 Properties: name, component, description
+	let patterns = [
+		{
+			name: 'Pattern 1',
+			component: Pattern1,
+			description: 'Pattern 1 description'
+		},
+		{
+			name: 'Pattern 2',
+			component: Pattern2,
+			description: 'Pattern 2 description'
+		},
+		{
+			name: 'Pattern 3',
+			component: Pattern3,
+			description: 'Pattern 3 description'
+		},
+		{
+			name: 'Pattern 4',
+			component: Pattern4,
+			description: 'Pattern 4 description'
+		}
+	];
+
+  // Reative State Variable mit dem Index fürs Pattern im Array patterns
+	let selectedPattern = $state(0);
+
+  // Property component vom selektierten Pattern, in die reaktivere Variable SelectedPattern schreiben.
+  // SelectedPattern Komponente wird unten mit <SelectedPattern /> geladen. Und gewechselt, wenn geklickt wird.
+	let SelectedPattern = $derived(patterns[selectedPattern].component);
 </script>
 
 <div class="app-container">
-  <Header />
+	<Header />
 
 	<main class="app-main">
-    <PatternNavigation />
-    <Pattern />
+		<div class="sidebar-left">
+
+      <!-- Schleife durch die Patterns und Buttons mit Events erstellen, um die Patterns umzuschalten. -->
+			{#each patterns as pattern, index}
+				<button
+					class="sidebar-left-item"
+					class:selected={selectedPattern === index}
+					onclick={() => (selectedPattern = index)}
+					>{pattern.name}
+					{#if selectedPattern === index}
+						<div transition:slide class="sidebar-left-description">{pattern.description}</div>
+					{/if}
+				</button>
+			{/each}
+		</div>
+
+    <!-- SelectedPattern rendern, wird automatisch mit state / derived geändert. -->
+		<SelectedPattern />
 	</main>
 
-  <Footer />
+	<Footer />
 </div>
+
+<!-- Style für die Sidebar mit items. -->
+<style>
+	.sidebar-left {
+		display: flex;
+		width: 350px;
+		flex-direction: column;
+		background: #1c1c1c;
+		border-right: 1px solid #353535;
+		/* padding: 20px; */
+		padding-top: 30px;
+		color: #fff;
+	}
+	.sidebar-left-item {
+		background: none;
+		color: #888;
+		border: none;
+		/* margin-bottom: 1rem; */
+		cursor: pointer;
+		text-align: left;
+		font-size: 0.9rem;
+		width: 100%;
+		padding: 10px 20px 10px 20px;
+		border-top: 1px solid #333;
+		letter-spacing: 0.02rem;
+	}
+	.sidebar-left-item:last-child {
+		border-bottom: 1px solid #333;
+	}
+	.sidebar-left-item:hover {
+		color: #aaa;
+	}
+	.sidebar-left-item.selected {
+		color: #ddd;
+	}
+	.sidebar-left-description {
+		font-size: 0.7rem;
+		color: #aaa;
+		margin-top: 0.5rem;
+		line-height: 1.4;
+	}
+</style>
